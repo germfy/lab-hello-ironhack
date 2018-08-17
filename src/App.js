1a47caafe26399794b2aa6f7530448d02bffa76d
mon0225 <elizabeta0225@hotmail.com>
2018-08-16 22:11:46 -0500

    Alejandro y Monserrat

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./App.css";
+class App extends Component{
+ render(){
+    return(
+    <div className="App">
+    <h1> Say hello to ReactJs </h1>
+    <p>"You will learn a Frontend framework from scratch, to becaome an Ninka Developer."</p>
+    <button>Awesome!</button>
+    </div>
+    );
+   }
+}
+export default App;
