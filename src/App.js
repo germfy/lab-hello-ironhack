f3661c9d52030741b0c84047b2a8fe52ac92a017
alekz7 <ing.aragon.ipn@gmail.com>
2018-08-19 21:52:45 -0500

    acomodando las imagenes del footer

--- src/App.js
+++ src/App.js
@@ -33,8 +33,45 @@ class App extends Component {
             Awesome!
           </a>
         </div>
-        <div className="image">
-          <img src="react-benefits.png" alt="React Benefits" />
+        <div className="footer">
+          <div className="row">
+            <div className="card col-xs-12 col-sm-3">
+              <img className="card-img-top" src="one.png" alt="Card image cap"></img>
+              <div className="card-body">
+                <h5 className="card-title">Declarative</h5>
+                <p className="card-text">React makes it</p>
+                <p className="card-text">painless to create</p>
+                <p className="card-text">interactive UIs</p>
+              </div>
+            </div>
+            <div className="card col-xs-12 col-sm-3">
+              <img className="card-img-top" src="two.png" alt="Card image cap"></img>
+              <div className="card-body">
+                <h5 className="card-title">Components</h5>
+                <p className="card-text">Build encapsulated</p>
+                <p className="card-text">components that</p>
+                <p className="card-text">manage their state</p>
+              </div>
+            </div>
+            <div className="card col-xs-12 col-sm-3">
+              <img className="card-img-top" src="tree.png" alt="Card image cap"></img>
+              <div className="card-body">
+                <h5 className="card-title">Single-Way</h5>
+                <p className="card-text">A set of immutable</p>
+                <p className="card-text">values are passed to</p>
+                <p className="card-text">the components</p>
+              </div>
+            </div>
+            <div className="card col-xs-12 col-sm-3">
+              <img className="card-img-top" src="four.png" alt="Card image cap"></img>
+              <div className="card-body">
+                <h5 className="card-title">JSX</h5>
+                <p className="card-text">Statically-typed,</p>
+                <p className="card-text">designed to run on</p>
+                <p className="card-text">modern browsers.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
