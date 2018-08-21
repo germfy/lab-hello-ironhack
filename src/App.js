b988b67c6ca85535ed8c349c8135e18a420c82ad
mon0225 <elizabeta0225@hotmail.com>
2018-08-20 21:14:47 -0500

    Alejandro y Monserrat PT-MEX-final

--- src/App.js
+++ src/App.js
@@ -4,9 +4,28 @@ class App extends Component{
  render(){
     return(
     <div className="App">
-    <h1> Say hello to ReactJs </h1>
-    <p>"You will learn a Frontend framework from scratch, to becaome an Ninka Developer."</p>
+    <div className="logo">
+      <img 
+        src="ironhack-logo.svg" 
+        alt="triangle with all three sides equal"/>
+    </div>
+ <h1>Say hello to</h1>
+ <h2>ReactJs</h2>
+   
+    <p>"You will learn a Frontend</p>
+    <p>framework from scratch, to</p>
+    <p>becaome an Ninka Developer."</p>
     <button>Awesome!</button>
+<div className="menu">
+<img 
+    src="menu-top.svg" 
+    alt="triangle with all three sides equal"/>
+</div>
+<div className="react">
+    <img 
+    src="react-logo.svg" 
+    alt="triangle with all three sides equal"/>
+</div>
     </div>
     );
    }
